7737f2b428dfe8948dd17b725f7df440b543467d
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-06 12:52:41 +0530

    Create Decimal to binary, octal and hexadecimal.py

---
+++ Day 3/Decimal to binary, octal and hexadecimal.py
@@ -0,0 +1,9 @@
+# Converting decimal number to binary, octal, and hexadecimal
+
+num = int(input("Enter any decimal number= "))
+bin_num = bin(num)
+oct_num = oct(num)
+hex_num = hex(num)
+print('Binary form of', num, 'is', bin_num)
+print('Octal form of', num, 'is', oct_num)
+print('Hexadecimal form of', num, 'is', hex_num)
