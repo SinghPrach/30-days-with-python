aa3235c282388970b9015cde49e969893180c788
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-19 13:41:00 +0530

    Create Print colored text in terminal.py

---
+++ Day 9/Print colored text in terminal.py
@@ -0,0 +1,7 @@
+#Using termcolor
+from termcolor import colored
+
+print(colored('My name is Prachi Singh', 'Green'))
+
+#Using ANSI
+print('\x1b[38;2;5;86;243m' + 'My name is Prachi Singh' + '\x1b[0m')
