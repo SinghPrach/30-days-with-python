c6fd25866b16400b99af5237f2114498b0dda51d
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-19 13:04:29 +0530

    Update check if list is empty.py

--- Day 9/check if list is empty.py
+++ Day 9/check if list is empty.py
@@ -5,3 +5,9 @@ if not my_list:
 else:
     print('The list is non-empty')
     
+#Using len()
+my_list = []
+if not len(my_list):
+    print('The list is empty')
+else:
+    print('The list is non-empty')
