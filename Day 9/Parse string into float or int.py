d24287c9eeb1bc36bb84312d939962c59ae2e91b
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-19 13:36:45 +0530

    Create Parse string into float or int.py

---
+++ Day 9/Parse string into float or int.py
@@ -0,0 +1,29 @@
+#String to int
+num_string = '2000'
+num_int = int(num_string)
+
+# print the type
+print(type(num_string))
+
+# print the value
+print(num_int)
+
+#String to float
+num_string = '2000.2'
+num_float = float(num_string)
+
+# print the type
+print(type(num_float))
+
+# print the value
+print(num_float)
+
+#Float to int
+num_string = '2000.2'
+num_int = int(float(num_string))
+
+# print the type
+print(type(num_int))
+
+# print the value
+print(num_int)
