8be4d4b998bd7c8201497cefb906130c8b0a6b8b
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-20 15:35:27 +0530

    Update random element from a list.py

--- Day 10/random element from a list.py
+++ Day 10/random element from a list.py
@@ -1,5 +1,11 @@
 #Using random
 import random
 
-my_list = [1, 'a', 32, 'c', 'd', 31]
+my_list = [1, 'a', 27, 'c', 'd', 31]
 print(random.choice(my_list))
+
+#Using secrets
+import secrets
+
+my_list = [1, 'a', 27, 'c', 'd', 31]
+print(secrets.choice(my_list))
