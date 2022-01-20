63c5f694d947232e48b31c06bff89ea967cf05fa
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-20 15:33:08 +0530

    Create Print output without a new line.py

---
+++ Day 10/Print output without a new line.py
@@ -0,0 +1,10 @@
+# print each statement on a new line
+print('I love')
+print('my country.')
+
+# new line
+print()
+
+# print both the statements on a single line
+print('I love', end=' ')
+print('my country.')
