9aaf8ef953df3d01996c2b5bcbb28d16a4107891
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-21 10:42:37 +0530

    Create occurrence of an item in a list.py

---
+++ Day 11/occurrence of an item in a list.py
@@ -0,0 +1,4 @@
+my_list = ['a', 1, 'a', 4, 2, 3, 4, 4, 2, 'a']
+item_to_check = input('Enter the item whose frequency you want to check: ')
+frequency_item = my_list.count(item_to_check)
+print(frequency_item)
