2894c1570e27ec602d7911a9f3223e6b32b73302
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-21 10:50:32 +0530

    Update create multiline string.py

--- Day 11/create multiline string.py
+++ Day 11/create multiline string.py
@@ -5,3 +5,11 @@ by writing code
 regularly and consistently.'''
 
 print(my_string)
+
+#Using \n and \
+my_string = 'The only way to \n'\
+            'learn to program is \n'\
+            'by writing code \n'\
+            'regularly and consistently.'
+
+print(my_string)
