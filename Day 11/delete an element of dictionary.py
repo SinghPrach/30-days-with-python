6b6a9714d04e985c18dcc48a1ae450d6f7d24c62
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-21 10:44:43 +0530

    Update delete an element of dictionary.py

--- Day 11/delete an element of dictionary.py
+++ Day 11/delete an element of dictionary.py
@@ -1,5 +1,13 @@
+#Using del
 my_dict = {31: 'Jan', 28: 'Feb', 30: 'April'}
 
 del my_dict[31]
+
+print(my_dict)
+
+#Using pop()
+my_dict = {31: 'Jan', 28: 'Feb', 30: 'April'}
+
+print(my_dict.pop(31))
 
 print(my_dict)
