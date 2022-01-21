278faff9ca8dbb5477106c08d1148d5c1075d728
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-21 10:39:48 +0530

    Update check if a string is float or int.py

--- Day 11/check if a string is float or int.py
+++ Day 11/check if a string is float or int.py
@@ -1,3 +1,4 @@
+#Check for float
 def check_float(num):
     try:
         float(num)
@@ -8,3 +9,15 @@ def check_float(num):
 
 value = input('Enter any value you want to check: ')
 print(check_float(value))
+
+#Check for int
+def check_int(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+
+value = input('Enter any value you want to check: ')
+print(check_int(value))
