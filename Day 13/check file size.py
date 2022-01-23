1057563b71df626833b8b4cec745cb66b0179e76
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-23 12:42:23 +0530

    Update check file size.py

--- Day 13/check file size.py
+++ Day 13/check file size.py
@@ -1,1 +1,11 @@
+#Using os module
+import os
 
+file_size = os.stat('data.txt')   #data.txt is the name of the file
+print(file_size.st_size)
+
+#Using pathlib module
+from pathlib import Path
+
+file = Path('data.txt')   #data.txt is the name of the file
+print(file.stat().st_size)
