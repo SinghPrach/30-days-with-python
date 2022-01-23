6d0a44f540d62189ca9fcd5fb5241b2947cf37dc
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-23 12:35:22 +0530

    Update Find All File with .txt Extension inside a directory.py

--- Day 13/Find All File with .txt Extension inside a directory.py
+++ Day 13/Find All File with .txt Extension inside a directory.py
@@ -7,3 +7,16 @@ for file in glob.glob("*.txt"):
     print(file)
 
 #Using os
+import os
+
+for file in os.listdir('directory_name'):
+    if file.endswith(".txt"):
+        print(file)
+        
+#Using os.walk
+import os
+
+for root, dirs, files in os.walk('directory_name'):
+    for file in files:
+        if file.endswith(".txt"):
+            print(file)
