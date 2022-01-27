10bb7932b0046479d9b04d5f33bf8e7c36d21e4c
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-27 16:29:48 +0530

    Update General multiple integration.py

--- Day 17/Scipy.integrate/General multiple integration.py
+++ Day 17/Scipy.integrate/General multiple integration.py
@@ -1,3 +1,18 @@
+#Program 1
+
 from scipy.integrate import quad, dblquad
 def I(n):
     return dblquad(lambda t, x: np.exp(-x*t)/t**n, 0, np.inf, lambda x: 1, lambda x: np.inf)
+
+#Program 2
+from scipy import integrate
+def f(x, y):
+    return x*y
+
+def bounds_y():
+    return [0, 0.5]
+
+def bounds_x(y):
+    return [0, 1-2*y]
+
+integrate.nquad(f, [bounds_x, bounds_y])
