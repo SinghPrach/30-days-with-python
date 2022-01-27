d1bf15be7b1291db61b41f3f85a65f791fdb9681
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-27 16:21:25 +0530

    Update and rename Day 17/file to Day 17/Scipy.integrate/General integration.py

---
+++ Day 17/Scipy.integrate/General integration.py
@@ -0,0 +1,11 @@
+import scipy.integrate as integrate
+import scipy.special as special
+
+result = integrate.quad(lambda x: special.jv(2.5, x), 0, 4.5)
+print(result)
+
+from numpy import sqrt, sin, cos, pi
+from scipy import special
+
+I = sqrt(2/pi)*(18.0/27*sqrt(2)*cos(4.5) - 4.0/27*sqrt(2)*sin(4.5) +
+                sqrt(2*pi) * special.fresnel(3/sqrt(pi))[0])
