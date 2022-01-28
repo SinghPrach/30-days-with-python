b46ea46f84be2c9a0143ecedfb12134009583c2a
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-01-28 14:02:07 +0530

    Create Newton-Conjugate-Gradient algorithm.py

---
+++ Day 18/scipy.optimize/Newton-Conjugate-Gradient algorithm.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.optimize import minimize, rosen, rosen_der
+
+
+def rosen_hess(x):
+    x = np.asarray(x)
+    H = np.diag(-400 * x[:-1], 1) - np.diag(400 * x[:-1], -1)
+    diagonal = np.zeros_like(x)
+    diagonal[0] = 1200 * x[0] ** 2 - 400 * x[1] + 2
+    diagonal[-1] = 200
+    diagonal[1:-1] = 202 + 1200 * x[1:-1] ** 2 - 400 * x[2:]
+    H = H + np.diag(diagonal)
+    return H
+
+
+res = minimize(rosen, x0, method='Newton-CG',
+               jac=rosen_der, hess=rosen_hess,
+               options={'xtol': 1e-8, 'disp': True})
+
+print(res.x)
