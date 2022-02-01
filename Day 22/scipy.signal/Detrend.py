2d2c0ec9a115dab2df7f214965d5d322d2430b3f
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-01 13:02:21 +0530

    Create Detrend.py

---
+++ Day 22/scipy.signal/Detrend.py
@@ -0,0 +1,15 @@
+# SciPy provides the function detrend to remove a constant or linear trend in a data series in order to see effect of higher order.
+
+import numpy as np
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+t = np.linspace(-10, 10, 20)
+y = 1 + t + 0.01*t**2
+yconst = signal.detrend(y, type='constant')
+ylin = signal.detrend(y, type='linear')
+plt.plot(t, y, '-rx')
+plt.plot(t, yconst, '-bo')
+plt.plot(t, ylin, '-k+')
+plt.grid()
+plt.legend(['signal', 'const. detrend', 'linear detrend'])
+plt.show()
