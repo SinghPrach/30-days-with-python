571ff92110dc365dce9b9fe0e3b4aeff7f12d4ed
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-05 16:15:49 +0530

    Update Word Ladders.py

--- Day 26/scipy.sparse.csgraph/Word Ladders.py
+++ Day 26/scipy.sparse.csgraph/Word Ladders.py
@@ -2,3 +2,10 @@ import numpy as np
 word_list = np.asarray(word_list)
 word_list.dtype   # these are unicode characters in Python 3
 word_list.sort()  # sort for quick searching later
+word_bytes = np.ndarray((word_list.size, word_list.itemsize),
+                        dtype='uint8',
+                        buffer=word_list.data)
+# each unicode character is four bytes long. We only need first byte
+# we know that there are three characters in each word
+word_bytes = word_bytes[:, ::word_list.itemsize//3]
+word_bytes.shape
