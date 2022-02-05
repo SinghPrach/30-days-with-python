a5834dc911111e8a6ad585bf5ca00c7e4bcf0a19
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-05 16:17:27 +0530

    Update Word Ladders.py

--- Day 26/scipy.sparse.csgraph/Word Ladders.py
+++ Day 26/scipy.sparse.csgraph/Word Ladders.py
@@ -9,3 +9,16 @@ word_bytes = np.ndarray((word_list.size, word_list.itemsize),
 # we know that there are three characters in each word
 word_bytes = word_bytes[:, ::word_list.itemsize//3]
 word_bytes.shape
+
+# Using Hamming distance between each point to determine which pairs of words are connected
+from scipy.spatial.distance import pdist, squareform
+from scipy.sparse import csr_matrix
+hamming_dist = pdist(word_bytes, metric='hamming')
+# there are three characters in each word
+graph = csr_matrix(squareform(hamming_dist < 1.5 / 3))
+i1 = word_list.searchsorted('ape')
+i2 = word_list.searchsorted('man')
+word_list[i1]
+# 'ape'
+word_list[i2]
+# 'man'
