210158bf6c7939a61b05f0ae6bc59ba3c07af364
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-06 09:58:50 +0530

    Create Convex hulls.py

---
+++ Day 27/scipy.spatial/Convex hulls.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
+
+rng = np.random.default_rng()
+points = rng.random((30, 2))   # 30 random points in 2-D
+hull = ConvexHull(points)
+
+
+plt.plot(points[:,0], points[:,1], 'o')
+for simplex in hull.simplices:
+    plt.plot(points[simplex,0], points[simplex,1], 'k-')
+plt.show()
