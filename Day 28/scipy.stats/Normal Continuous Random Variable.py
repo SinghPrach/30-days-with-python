50ea22cfa46c79fd088661149bbf47c91b7e7714
SinghPrach <73215429+SinghPrach@users.noreply.github.com>
2022-02-07 11:45:28 +0530

    Update and rename Day 28/file to Day 28/scipy.stats/Normal Continuous Random Variable.py

---
+++ Day 28/scipy.stats/Normal Continuous Random Variable.py
@@ -0,0 +1,12 @@
+# A probability distribution in which the random variable X can take any value is continuous random variable.
+
+from scipy.stats import norm
+import numpy as np
+print norm.cdf(np.array([1,-1., 0, 1, 3, 4, -2, 6]))
+
+# To find the median of a distribution, we can use the Percent Point Function (ppf).
+
+from scipy.stats import norm
+print norm.ppf(0.5)
+
+
